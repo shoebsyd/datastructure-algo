cf9fc568f58b86dfcd6781024f7cc06dfab30dfc
Shoeb <invokevirtual32@gmail.com>
2020-04-26 18:17:25 +0530

    Update swap method to not swap equal elements

--- src/main/java/com/algo/BubbleSort.java
+++ src/main/java/com/algo/BubbleSort.java
@@ -20,13 +20,16 @@ public class BubbleSort {
                 if(array[i] > array[i+1])
                     swap(array, i, i+1);
             }
-
         }
 
         return array;
     }
 
     private static void swap(int[] array, int i, int j) {
+
+        if(i == j)
+            return;
+
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
