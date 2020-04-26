822b4b847ce86d5abc840b4a07af1b73cfcc71b0
Shoeb <invokevirtual32@gmail.com>
2020-04-26 18:08:35 +0530

    Add additional test

--- src/test/java/com/algo/BubbleSortTest.java
+++ src/test/java/com/algo/BubbleSortTest.java
@@ -5,11 +5,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class BubbleSortTest {
-:wq!
+
     @Test
     public void testSort() {
         int[] inputArray = {12, 35, -1, 1, 55, 10};
         int[] expectedArray = {-1, 1, 10, 12, 35, 55};
+        int[] sortedArray = new BubbleSort().sort(inputArray);
+        assertArrayEquals(expectedArray, sortedArray);
+    }
+
+    @Test
+    public void testSortWithEqualNumbers() {
+        int[] inputArray = {12, 35, -1, 1, 55, -1, 35, 0, 100, -100, 10};
+        int[] expectedArray = {-100, -1, -1, 0, 1, 10, 12, 35, 35, 55, 100};
         int[] sortedArray = new BubbleSort().sort(inputArray);
         assertArrayEquals(expectedArray, sortedArray);
     }
