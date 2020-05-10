f53f7a9db3efb733b5c2e5aca1e6d678ba777493
Shoeb <invokevirtual32@gmail.com>
2020-05-11 02:46:30 +0530

    Add ShellSort algo

--- src/main/java/com/algo/ShellSort.java
+++ src/main/java/com/algo/ShellSort.java
@@ -1,28 +1,33 @@
 package com.algo;
 
-public class InsertionSort {
+public class ShellSort {
 
     public static void main(String[] args) {
 
         int[] array = {12, 35, -1, 1, 55, 10};
-        int[] sortedArray = new InsertionSort().sort(array);
+        int[] sortedArray = new ShellSort().sort(array);
         printArray(sortedArray);
 
     }
 
     public int[] sort(int[] array) {
 
-        for(int firstUnsortedIndex = 1; firstUnsortedIndex < array.length;
-            firstUnsortedIndex++) {
+        for(int gap = array.length/2; gap > 0; gap = gap/2) {
 
-            int newElement = array[firstUnsortedIndex];
+            for(int i = gap; i < array.length; i++) {
 
-            int i;
-            for(i = firstUnsortedIndex; i > 0 && array[i - 1] > newElement; i--) {
-               array[i] = array[i - 1];
+                int newElement = array[i];
+                int j = i;
+
+                while(j >= gap && array[j - gap] > newElement) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+
+                array[j] = newElement;
+
             }
 
-            array[i] = newElement;
         }
 
         return array;
--- src/test/java/com/algo/ShellSortTest.java
+++ src/test/java/com/algo/ShellSortTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 
-public class InsertionSortTest {
+public class ShellSortTest {
 
     @Test
     public void testSort() {
         int[] inputArray = {12, 35, -1, 1, 55, 10};
         int[] expectedArray = {-1, 1, 10, 12, 35, 55};
-        int[] sortedArray = new InsertionSort().sort(inputArray);
+        int[] sortedArray = new ShellSort().sort(inputArray);
         assertArrayEquals(expectedArray, sortedArray);
     }
 
@@ -18,7 +18,7 @@ public class InsertionSortTest {
     public void testSortWithEqualNumbers() {
         int[] inputArray = {12, 35, -1, 1, 55, -1, 35, 0, 100, -100, 10};
         int[] expectedArray = {-100, -1, -1, 0, 1, 10, 12, 35, 35, 55, 100};
-        int[] sortedArray = new InsertionSort().sort(inputArray);
+        int[] sortedArray = new ShellSort().sort(inputArray);
         assertArrayEquals(expectedArray, sortedArray);
     }
 
